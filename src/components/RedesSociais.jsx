77ae7591706d3815fed77bc4850998de47fe57a2
gustavofda <gustavoaraujo2017fise@gmil.com>
2024-07-26 06:04:42 -0300

    atualização

--- src/components/RedesSociais.jsx
+++ src/components/RedesSociais.jsx
@@ -6,19 +6,19 @@ const RedesSociais = () => (
     <h2 className="secondary-title mb-m">Redes Sociais</h2>
     <ul>
       <li>
-        <a href="https://www.linkedin.com/in/seu-perfil" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/gustavofda" target="_blank" rel="noopener noreferrer">
           <FaLinkedin />
         </a>
         <p>LinkedIn</p>
       </li>
       <li>
-        <a href="https://github.com/seu-perfil" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/gustavofda" target="_blank" rel="noopener noreferrer">
           <FaGithub />
         </a>
         <p>GitHub</p>
       </li>
       <li>
-        <a href="https://twitter.com/seu-perfil" target="_blank" rel="noopener noreferrer">
+        <a href="https://twitter.com/haoshuku" target="_blank" rel="noopener noreferrer">
           <FaTwitter />
         </a>
         <p>Twitter</p>
